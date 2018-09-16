1a34d59a7c675f087af8becf0cb9e46bcf1f3b22
David Araya <davidaraya@Davids-MacBook-Pro.local>
2018-09-16 12:04:37 -0400

    change: created the implementation

--- src/test/java/core/ArithmeticSolverTest.java
+++ src/test/java/core/ArithmeticSolverTest.java
@@ -8,30 +8,30 @@ public class ArithmeticSolverTest extends TestCase{
 	public void testAdd() {
 		ArithmeticSolver solver = new ArithmeticSolver();
 		
-		assertEquals(4, solver.add(2, 2)); // 2 + 2 = 4
+		assertEquals(4.0, solver.add(2, 2)); // 2 + 2 = 4
 	}
 	
 	public void testSubstract() {
 		ArithmeticSolver solver = new ArithmeticSolver();
 		
-		assertEquals(2, solver.substract(4, 2)); // 4 - 2 = 2
+		assertEquals(2.0, solver.substract(4, 2)); // 4 - 2 = 2
 	}
 	
 	public void testMultiply() {
 		ArithmeticSolver solver = new ArithmeticSolver();
 	
-		assertEquals(16, solver.multiply(8, 2)); // 8 * 2 = 8
+		assertEquals(16.0, solver.multiply(8, 2)); // 8 * 2 = 8
 	}
 
 	public void testDivide() {
 		ArithmeticSolver solver = new ArithmeticSolver();
 	
-		assertEquals(3, solver.divide(9, 3)); // 9 / 3 = 3
+		assertEquals(3.0, solver.divide(9, 3)); // 9 / 3 = 3
 	}
 	
 	public void testModulus() {
 		ArithmeticSolver solver = new ArithmeticSolver();
 	
-		assertEquals(1, solver.modulus(5, 2)); // 5 % 2 = 1
+		assertEquals(1.0, solver.modulus(5, 2)); // 5 % 2 = 1
 	}
 }
